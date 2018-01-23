402dea23b07bd9b54d303aaf22bf1f4f998a1e29
snow-R3 <atulkumarkumar166@gmail.com>
2018-01-23 16:22:13 -0500

    Added backend api for userinfo update

--- src/container/bossinfo/bossinfo.js
+++ src/container/bossinfo/bossinfo.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { NavBar, InputItem, TextareaItem, Button } from 'antd-mobile';
 import { connect } from 'react-redux';
+import {Redirect} from 'react-router-dom'
 
 import AvatarSelector from '../../component/avatar-selector/avatar-selector';
 import { update } from '../../redux/user.redux';
@@ -26,8 +27,11 @@ class BossInfo extends Component {
 	}
 
   render () {
+    const path = this.props.location.pathname
+		const redirect = this.props.redirectTo
     return (
       <div>
+        {redirect&&redirect!==path? <Redirect to={this.props.redirectTo}></Redirect> :null}
         <NavBar
           mode="dark"
         >Boss Profile</NavBar>
