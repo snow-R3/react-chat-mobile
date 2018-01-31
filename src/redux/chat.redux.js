e13e05db49578d674a656bb03bbcc9570214bd6a
snow-R3 <atulkumarkumar166@gmail.com>
2018-01-31 10:37:05 -0500

    Get user avatar info

--- src/redux/chat.redux.js
+++ src/redux/chat.redux.js
@@ -12,6 +12,7 @@ const MSG_READ = 'MSG_READ';
 
 const initState = {
   chatmsg: [],
+  users: {},
   unread: 0
 }
 
@@ -20,8 +21,9 @@ export function chat(state=initState, action) {
     case MSG_LIST:
       return {
         ...state,
-        chatmsg: action.payload,
-        unread: action.payload.filter(v => !v.read).length
+        chatmsg: action.payload.msgs,
+        users: action.payload.users,
+        unread: action.payload.msgs.filter(v => !v.read).length
       }
     case MSG_RECV:
       const n = action.payload.to === action.userid ? 1 : 0;
@@ -36,8 +38,8 @@ export function chat(state=initState, action) {
   }
 }
 
-function msgList(msgs) {
-  return {type: MSG_LIST, payload: msgs}
+function msgList(msgs, users) {
+  return {type: MSG_LIST, payload: {msgs, users}}
 }
 
 function msgRecv(msg) {
@@ -65,7 +67,7 @@ export function getMsgList() {
     axios.get('/user/getMsgList')
       .then(res => {
         if (res.status === 200 && res.data.code === 0) {
-          dispatch(msgList(res.data.msgs))
+          dispatch(msgList(res.data.msgs, res.data.users))
         }
       })
   }
